95b76ba64ff4a74f815abec364dc24cb0bd18412
coinhat <nhatcoifacebook@gmail.com>
2024-11-02 18:09:42 +0700

    build gradle

---
+++ src/main/java/com/example/petmount/PetMountApplication.java
@@ -0,0 +1,13 @@
+package com.example.petmount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMountApplication.class, args);
+	}
+
+}
