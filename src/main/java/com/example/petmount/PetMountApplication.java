015758b178f4bc6b2fde13554b95ec9ad132d799
coinhat <nhatcoifacebook@gmail.com>
2024-11-04 13:42:15 +0700

    feat: configure env file to connect db

--- src/main/java/com/example/petmount/PetMountApplication.java
+++ src/main/java/com/example/petmount/PetMountApplication.java
@@ -1,5 +1,6 @@
 package com.example.petmount;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetMountApplication {
 
 	public static void main(String[] args) {
+
+		// Simple way to configure env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(PetMountApplication.class, args);
 	}
 
