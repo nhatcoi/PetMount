5045f50e3c5a10ce7bc821ddb8d672735e7948d4
coinhat <nhatcoifacebook@gmail.com>
2024-11-10 14:59:38 +0700

    fix: return pet response

--- src/main/java/com/example/petmount/controlles/PetController.java
+++ src/main/java/com/example/petmount/controlles/PetController.java
@@ -1,6 +1,7 @@
 package com.example.petmount.controlles;
 
 import com.example.petmount.dtos.PetDTO;
+import com.example.petmount.dtos.PetResponse;
 import com.example.petmount.services.PetService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,9 +22,8 @@ public class PetController {
 
     @GetMapping
     public ResponseEntity<?> getAllPets() {
-        return ResponseEntity.ok().body(
-                petService.findAll().stream()
+        return ResponseEntity.ok(new PetResponse(petService.findAll().size(), petService.findAll().stream()
                 .map(pet -> modelMapper.map(pet, PetDTO.class))
-        );
+                .collect(Collectors.toList())));
     }
 }
