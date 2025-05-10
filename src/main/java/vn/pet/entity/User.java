2d7fe24c101a0f6e6f1f9acc2237487c334a6125
nhatcoi <nhatcoifacebook@gmail.com>
2025-05-10 14:33:32 +0700

    add entity

--- src/main/java/vn/pet/entity/User.java
+++ src/main/java/vn/pet/entity/User.java
@@ -1,13 +1,18 @@
 package vn.pet.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import vn.pet.entity.BaseEntity.BaseEntity;
 import vn.pet.util.Gender;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import vn.pet.util.UserStatus;
+import vn.pet.util.UserType;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -32,6 +37,9 @@ public class User extends BaseEntity {
     @Column(name = "username")
     private String username;
 
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "dob")
     private Date dob;
 
@@ -40,4 +48,18 @@ public class User extends BaseEntity {
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private Gender gender;
 
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private UserStatus status;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private UserType type;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Set<Address> addresses = new HashSet<>();
 }
